d830e5b3943648fdf246ed82e53010a2f18e15f8
Wilthos <wilthos87@gmail.com>
2025-01-09 20:58:48 -0700

    All but last part of tutorial completed + mods
    All parts completed except the last tutorial. Also, I modded it so you have to press space to shoot and can hold space for auto fire. I also made it to where you laser dies when it hits an enemy (prior, it would continue on after hitting one enemy and damage others)

--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -8,21 +8,36 @@ extends Node2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $Anchor/AnimatedSprite2D
 @onready var move_component: MoveComponent = $MoveComponent
 @onready var flame_animated_sprite: AnimatedSprite2D = %FlameAnimatedSprite
+@onready var variable_pitch_audio_stream_player: VariablePitchAudioStreamPlayer = $VariablePitchAudioStreamPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	fire_rate_timer.timeout.connect(fire_lasers)
+	#fire_rate_timer.timeout.connect(fire_lasers)
+	
 	pass
 	
 
 func fire_lasers() -> void:
+	variable_pitch_audio_stream_player.play_with_variance()
 	spawner_component.spawn(left_muzzle.global_position)
 	spawner_component.spawn(right_muzzle.global_position)
 	scale_component.tween_scale()
 
 func _process(delta: float) -> void:
 	animate_the_ship()
+	if Input.is_action_just_pressed("Fire"):
+		fire_lasers()
+		fire_rate_timer.start()
+		if Input.is_action_pressed("Fire"):
+			fire_rate_timer.timeout.connect(fire_lasers)
+	if Input.is_action_just_released("Fire"):
+		fire_rate_timer.stop()
+		#if fire_rate_timer.is_stopped():
+			#fire_lasers()
+			#fire_rate_timer.start()
+		
+		
 	
 	
 func animate_the_ship() -> void:
