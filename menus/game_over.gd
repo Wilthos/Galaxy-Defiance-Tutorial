a99b151edfc409d37dee13b69210be4c39f43f0c
Wilthos <wilthos87@gmail.com>
2025-01-13 18:36:13 -0700

    Galaxy Defiance Tutorial Complete
    Completed the Galaxy Defiance Tutorial + my added changed https://youtube.com/playlist?list=PL9FzW-m48fn09w6j8NowI_pSBVcsb3V78&si=v1kv0JeimPv1H5qh

--- menus/game_over.gd
+++ menus/game_over.gd
@@ -1,14 +1,20 @@
 extends Control
 
+const SAVE_PATH = "user://save.cfg"
+const TEST_SAVE_PATH = "res://save.cfg"
+
 @export var game_stats: GameStats
+
+var save_path = TEST_SAVE_PATH
 
 @onready var score_value: Label = %ScoreValue
 @onready var high_score_value: Label = %HighScoreValue
 
 func _ready() -> void:
+	load_highscore()
 	if game_stats.score > game_stats.highscore:
 		game_stats.highscore = game_stats.score
-	
+		save_highscore()
 	score_value.text = str(game_stats.score)
 	high_score_value.text = str(game_stats.highscore)
 
@@ -17,3 +23,15 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		game_stats.score = 0
 		get_tree().change_scene_to_file("res://world.tscn")
+
+func load_highscore() -> void:
+	var config = ConfigFile.new()
+	var error = config.load(save_path)
+	if error != OK: return
+	game_stats.highscore = config.get_value("game", "highscore")
+	
+func save_highscore() -> void:
+	var config = ConfigFile.new()
+	config.set_value("game","highscore", game_stats.highscore)
+	config.save(save_path)
+	
